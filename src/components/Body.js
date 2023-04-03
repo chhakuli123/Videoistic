015ef4275fcf063aaf88f0e93391ad0ed7e91174
Chhakuli <chhakulizingare12@gmail.com>
2023-04-03 20:56:00 +0530

    feat: Implement nested routing with Outlet

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import MainContainer from "./MainContainer";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex">
-      <Sidebar />
-      <div className="w-full">
-        <MainContainer />
+      <div className="sticky top-0 h-screen">
+        <Sidebar />
       </div>
+      <Outlet />
     </div>
   );
 };
