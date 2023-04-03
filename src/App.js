323f3ee1750983410a32f5900d99018c735c503f
Chhakuli <chhakulizingare12@gmail.com>
2023-04-03 20:53:33 +0530

    feat: Add react-router-dom for client-side routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import "./App.css";
 import Body from "./components/Body";
 import Head from "./components/Head";
 import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 function App() {
   return (
     <Provider store={store}>
       <div>
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
