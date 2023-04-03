5c4e8480e23460e5ae7cfef7ed76127e40995072
Chhakuli <chhakulizingare12@gmail.com>
2023-04-03 21:07:39 +0530

    feat: Add YouTube API fetch in VideoCard

---
+++ src/components/VideoContainer.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
+
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    setVideos(json.items);
+  };
+  return (
+    <div className="flex flex-wrap items-center justify-center h-screen overflow-y-auto">
+      {videos.map((video) => (
+        <Link to={"/watch?v=" + video.id}>
+          <VideoCard key={video.id} info={video} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
