f1e3643ae2de5bfe1886d463383fbe43e9b136b1
Chhakuli <chhakulizingare12@gmail.com>
2023-04-05 18:27:46 +0530

    feat: Added Shimmer effect

--- src/components/VideoSuggestions.js
+++ src/components/VideoSuggestions.js
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from "react";
 
+// Component that renders a shimmer effect while data is loading
+const Shimmer = () => {
+  return (
+    <div className="flex items-center p-3">
+      <div className="w-[10rem] h-[6rem] bg-gray-300 rounded-xl animate-pulse"></div>
+      <div className="ml-3 mb-4">
+        <div className="w-40 h-4 bg-gray-300 rounded animate-pulse mb-1"></div>
+        <div className="w-20 h-4 bg-gray-300 rounded animate-pulse mb-1"></div>
+        <div className="w-14 h-4 bg-gray-300 rounded animate-pulse mb-1"></div>
+        <div className="w-8 h-4 bg-gray-300 rounded animate-pulse"></div>
+      </div>
+    </div>
+  );
+};
+
+// Component that fetches and renders video suggestions related to a videoId prop passed in
 const VideoSuggestions = ({ videoId }) => {
+  // State hook to hold the video suggestions
   const [suggestions, setSuggestions] = useState([]);
 
+  // Effect hook that fetches video suggestions on mount and whenever the videoId prop changes
   useEffect(() => {
     const fetchVideoSuggestions = async () => {
       try {
         const response = await fetch(
+          // Use the YouTube Data API to fetch related video suggestions based on the videoId prop passed in
           `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&key=AIzaSyDOw2x1wNwuU9PdYi3ij5Eoc0C-UpsakcU&maxResults=50`
         );
         const data = await response.json();
@@ -19,6 +38,7 @@ const VideoSuggestions = ({ videoId }) => {
     fetchVideoSuggestions();
   }, [videoId]);
 
+  // Render the video suggestions
   return (
     <div className="mb-6 ">
       <h2 className="text-xl font-bold mb-4">Video Suggestions</h2>
@@ -49,7 +69,12 @@ const VideoSuggestions = ({ videoId }) => {
             </li>
           ))
         ) : (
-          <li className="p-3 text-gray-600">No suggestions found.</li>
+          // If no suggestions have been loaded yet, show a shimmer effect to indicate that data is loading
+          <div className="p-3 text-gray-600">
+            {Array.from({ length: 20 }).map((_, index) => (
+              <Shimmer key={index} />
+            ))}
+          </div>
         )}
       </ul>
     </div>
