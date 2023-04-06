02806e0d9fbada83d6ece1c774093db3a95a5877
Chhakuli <chhakulizingare12@gmail.com>
2023-04-06 18:53:51 +0530

    reafactor:changes in constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=AIzaSyAqhBIMnttyRaLS77aUwBMT08G7Sfvgxc0";
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=" +
+  GOOGLE_API_KEY;
 
 export const VIDEO_INFO =
   "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDOw2x1wNwuU9PdYi3ij5Eoc0C-UpsakcU&maxResults=50&part=snippet&id=";
