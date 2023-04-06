e307d690268345b6ef78b741e342ad535e5087cb
Chhakuli <chhakulizingare12@gmail.com>
2023-04-06 09:31:55 +0530

    feat: Modified function with try-catch block

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,24 +1,37 @@
+// Import necessary modules
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 import VideoCard from "./VideoCard";
 import { Link } from "react-router-dom";
 
+// Component that fetches and renders the videos
 const VideoContainer = () => {
+  // Initialize state variables for videos and loading status
   const [videos, setVideos] = useState([]);
+
+  // Fetch videos on component mount
   useEffect(() => {
     getVideos();
   }, []);
 
+  // Fetch videos from YouTube API
   const getVideos = async () => {
-    const data = await fetch(YOUTUBE_VIDEOS_API);
-    const json = await data.json();
-    setVideos(json.items);
+    try {
+      const data = await fetch(YOUTUBE_VIDEOS_API);
+      const json = await data.json();
+      // Set videos and loading status when data is retrieved successfully
+      setVideos(json.items);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
+    // Render videos or shimmer effect based on loading status
     <div className="flex flex-wrap items-center justify-center h-screen overflow-y-auto">
       {videos.map((video) => (
-        <Link to={"/watch?v=" + video.id}>
-          <VideoCard key={video.id} info={video} />
+        <Link to={"/watch?v=" + video.id} key={video.id}>
+          <VideoCard info={video} />
         </Link>
       ))}
     </div>
