833cc0038f103b43a8ee9037d4d70872b0eb5f93
Chhakuli <chhakulizingare12@gmail.com>
2023-04-06 18:47:43 +0530

    refactor: Modified the api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=" +
-  GOOGLE_API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=AIzaSyAqhBIMnttyRaLS77aUwBMT08G7Sfvgxc0";
 
 export const VIDEO_INFO =
   "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDOw2x1wNwuU9PdYi3ij5Eoc0C-UpsakcU&maxResults=50&part=snippet&id=";
+
+export const SUGGESTIONS_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
