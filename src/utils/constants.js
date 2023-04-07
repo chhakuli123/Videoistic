092ee805803565cba1a319616f677ebedcbd6500
Chhakuli <chhakulizingare12@gmail.com>
2023-04-07 08:46:32 +0530

    refactor: Added key publicaly

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+// export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+const GOOGLE_API_KEY = "AIzaSyDuskSjcctnvIH42Yj7gPU28OOClCpuvwo";
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=" +
   GOOGLE_API_KEY;
