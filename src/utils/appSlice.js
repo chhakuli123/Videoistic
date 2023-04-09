c7ced8e4b794e8eefaf34efa4a192c25110990b4
Chhakuli <chhakulizingare12@gmail.com>
2023-04-09 16:27:43 +0530

    feat: Added useful comments

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -1,3 +1,7 @@
+// This code defines a Redux slice called "app" that manages the state of whether the app menu is open or closed. It sets the initial state to "isMenuOpen: true", and provides two reducer functions, "toggleMenu" and "closeMenu", that can be used to update this state.
+
+// When "toggleMenu" is called, it toggles the value of "isMenuOpen" from true to false, or vice versa. When "closeMenu" is called, it sets "isMenuOpen" to false.
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const appSlice = createSlice({
