5944363624fd5a6c6aab3fe94e12e8966dd023cc
Chhakuli <chhakulizingare12@gmail.com>
2023-04-09 16:24:27 +0530

    feat: Added useful comments

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,10 +2,14 @@ import React from "react";
 import ButtonsList from "./ButtonsList";
 import VideoContainer from "./VideoContainer";
 
+// Main container component which renders the ButtonsList and VideoContainer components
 const MainContainer = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center ml-12">
+      {/* Render the ButtonsList component */}
       <ButtonsList />
+
+      {/* Render the VideoContainer component */}
       <VideoContainer />
     </div>
   );
