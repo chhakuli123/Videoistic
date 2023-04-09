c1f5a56fe942f723316b11ac5c08e16f83d99f32
Chhakuli <chhakulizingare12@gmail.com>
2023-04-09 16:19:26 +0530

    feat: Added useful comments in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,12 @@ import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
+    // Use the flex display to arrange the sidebar and outlet in a row
     <div className="flex">
-      <div>
-        <Sidebar />
-      </div>
+      {/*Render the Sidebar component on the left side */}
+      <Sidebar />
+      {/* Use the Outlet component from react-router-dom to render the nested routes
+         defined in the parent component */}
       <Outlet />
     </div>
   );
