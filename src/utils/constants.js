20cdaca6d0b9bca1d1e5571a1ecd313893c6eece
Chhakuli <chhakulizingare12@gmail.com>
2023-04-09 16:28:38 +0530

    feat: Added useful comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-export const GOOGLE_API_KEY = "AIzaSyDuskSjcctnvIH42Yj7gPU28OOClCpuvwo";
+export const GOOGLE_API_KEY = "AIzaSyCkgq8eqpICtoav7WN2ySfFe6XWNJ_YWkY";
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=" +
   GOOGLE_API_KEY;
@@ -9,3 +9,6 @@ export const VIDEO_INFO =
 
 export const SUGGESTIONS_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const SEARCH_API =
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
