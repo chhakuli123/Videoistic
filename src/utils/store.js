ad6f3e69ff3518d46c96c36ddf1debe685e00d7e
Chhakuli <chhakulizingare12@gmail.com>
2023-04-09 16:29:37 +0530

    feat: Added Slice in store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
+import searchSlice from "./searchSlice";
+import categoreSice from "./categoreSice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
+    search: searchSlice,
+    categories: categoreSice,
   },
 });
 export default store;
